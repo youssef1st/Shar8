e19b7844a88cc47a4c296451e1b2af7b09855fe5
Irq <yossefasmajjhaha@gmail.com>
2026-09-23 21:50:53 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'shar8-v1';
+const assets = [
+  'shar8.html',
+  'data.json',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
